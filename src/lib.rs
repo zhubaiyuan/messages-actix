55f563ed1c925b2bc910479851a26e1e2b5a8da0
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-08-16 11:36:43 +0800

    add behaviors new and run to struct MessageApp

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,21 @@ use serde::Serialize;
 pub struct MessageApp {
     port: u16,
 }
+
+impl MessageApp {
+    pub fn new(port: u16) -> Self {
+        MessageApp { port }
+    }
+
+    pub fn run(&self) -> std::io::Result<()> {
+        println!("Starting http server: 127.0.0.1:{}", self.port);
+        HttpServer::new(move || {
+            App::new()
+                .wrap(middleware::Logger::default())
+                .service(index)
+        })
+        .bind(("127.0.0.1", self.port))?
+        .workers(8)
+        .run()
+    }
+}
